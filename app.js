a1ecd1eeca20d5ac4b86052e6568b57b709f662b
gopichandchalla16 <162360009+gopichandchalla16@users.noreply.github.com>
2024-06-20 12:54:22 +0530

    app.js

--- app.js
+++ app.js
@@ -1,15 +1,68 @@
-let menu = document.querySelector('#menu-bar');
-    let head = document.querySelector('.head .navbar');
+let menu = document.querySelector("#menu-bars");
+let navbar = document.querySelector(".navbar");
 
-    menu.onclick = () => {
-        head.classList.toggle('active');
-    };
+menu.onclick = () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+};
 
-    window.onscroll = () => {
-        head.classList.remove('active');
-        if (window.scrollY > 60) {
-            document.querySelector('#menu-bar').classList.add('active');
-        } else {
-            document.querySelector('#menu-bar').classList.remove('active');
-        }
-    };
+let themeToggler = document.querySelector(".theme-toggler");
+let toggleBtn = document.querySelector(".toggle-btn");
+
+toggleBtn.onclick = () => {
+  themeToggler.classList.toggle("active");
+};
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+  themeToggler.classList.remove("active");
+};
+
+document.querySelectorAll(".theme-toggler .theme-btn").forEach((btn) => {
+  btn.onclick = () => {
+    let color = btn.style.background;
+    document.querySelector(":root").style.setProperty("--theme-color", color);
+  };
+});
+
+var swiper = new Swiper(".home-slider", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: true,
+  },
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+});
+
+var swiper = new Swiper(".review-slider", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  spaceBetween: 10,
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    700: {
+      slidesPerView: 2,
+    },
+    1050: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+});
